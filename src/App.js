e0bdb9ab866c8b6f09333fa1e36290a751d27d9c
Santhosh Kummar <thisis.santhosh777@gmail.com>
2023-03-04 05:35:50 +0000

    first practice on lists and keys using react.js

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import UserProfile from './components/UserProfile'
+import './App.css'
+
+const userDetailsList = [
+  {
+    uniqueNo: 1,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'Santhosh kumar',
+    role: 'Frontend Developer',
+  },
+  {
+    uniqueNo: 2,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'Santhosh kumar',
+    role: 'Frontend Developer',
+  },
+  {
+    uniqueNo: 3,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'Santhosh kumar',
+    role: 'Frontend Developer',
+  },
+  {
+    uniqueNo: 4,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'Santhosh kumar',
+    role: 'Frontend Developer',
+  },
+]
+const App = () => (
+  <div className="lis-container">
+    <h1 className="title">Users List</h1>
+    <ul>
+      {userDetailsList.map(eachItem => (
+        <UserProfile userDetails={eachItem} key={eachItem.uniqueNo} />
+      ))}
+    </ul>
+  </div>
+)
+
+export default App
